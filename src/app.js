e2354210cfa964597fc77b1f5738d18fc8f6cdcc
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2024-10-31 13:17:49 +0900

    feat : change password

--- src/app.js
+++ src/app.js
@@ -7,24 +7,27 @@ const PORT = 8080;
 
 app.use(cors());
 
-// 정적 파일 경로 설정
-app.use(express.static(path.join(__dirname)));  // src 디렉토리를 정적 파일로 제공
+app.use(express.static(path.join(__dirname)));
 
 // 기본 라우트 설정
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "main", "index.html"));  // index.html 파일 경로
+  res.sendFile(path.join(__dirname, "pages", "main", "index.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "login", "index.html"));  // index.html 파일 경로
+  res.sendFile(path.join(__dirname, "pages", "login", "index.html"));
 });
 
 app.get("/sign-up", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "signup", "index.html"));  // index.html 파일 경로
+  res.sendFile(path.join(__dirname, "pages", "signup", "index.html"));
 });
 
 app.get("/modify-profile", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "modifyProfile", "index.html"));  // index.html 파일 경로
+  res.sendFile(path.join(__dirname, "pages", "modifyProfile", "index.html"));
+});
+
+app.get("/change-password", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "changePassword", "index.html"));
 });
 
 app.listen(PORT, () => {
