0a90943c8ac7e38ed7d4d902ff95765764476a67
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2024-11-14 17:50:41 +0900

    refacotr: migration react

--- src/app.js
+++ src/app.js
@@ -1,21 +1,63 @@
-const express = require("express");
-const path = require("path");
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Container from './layouts/Container';
+import Main from './views/Main';
+import SignUp from './views/SignUp';
+import User from './views/User';
+import Login from './views/Login';
+import Setting from './views/Setting';
+import { ProtectedRoute } from './components/util/ProtectedRoute';
+import { RedirectRoute } from './components/util/RedirectRoute';
+import { useSelector, useDispatch } from 'react-redux';
+import { userInfoRequest } from './apis/user';
+import { logoutUser, initUser } from './stores/userSlice';
+import { 
+        MAIN_PATH, 
+        LOGIN_PATH, 
+        SIGNUP_PATH, 
+        USER_PATH, 
+        SETTING_PATH, 
+        WRITE_POST_PATH 
+    } from './constants';
+import WritePost from './views/WritePost';
 
-const app = express();
-const PORT = 8080;
+function App() {
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
-// 정적 파일 경로 설정
-app.use(express.static(path.join(__dirname)));  // src 디렉토리를 정적 파일로 제공
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            if (isLoggedIn) {
+                const userInfoResponse = await userInfoRequest();
+                if (userInfoResponse) {
+                    dispatch(initUser({
+                        name: userInfoResponse.user.name,
+                        email: userInfoResponse.user.email,
+                        profile_url: userInfoResponse.user.profile_url,
+                    }));
+                } else {
+                    dispatch(logoutUser());
+                }
+            }
+        };
+        
+        fetchUserInfo();
+    }, [isLoggedIn, dispatch]);
 
-// 기본 라우트 설정
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "main", "index.html"));  // index.html 파일 경로
-});
+    return (
+        <Routes>
+            <Route element={<Container />}>
+                <Route path={MAIN_PATH()} element={<Main />} />
+                <Route path={USER_PATH(`:userName`)} element={<User />} />
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "login", "index.html"));  // index.html 파일 경로
-});
+                <Route path={LOGIN_PATH()} element={<RedirectRoute><Login /></RedirectRoute>} />
+                <Route path={SIGNUP_PATH()} element={<RedirectRoute><SignUp /></RedirectRoute>} />
 
-app.listen(PORT, () => {
-  console.log(`Frontend server running on http://localhost:${PORT}`);
-});
+                <Route path={SETTING_PATH()} element={<ProtectedRoute><Setting /></ProtectedRoute>} />
+                <Route path={WRITE_POST_PATH()} element={<ProtectedRoute><WritePost /></ProtectedRoute>} />
+            </Route>
+        </Routes>
+    );
+}
+
+export default App;
